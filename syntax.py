9f6cdb0339d1f10c2a1d2bbb406a95ef5ef23081
S-Pz <savio_fr@outlook.com>
2024-08-27 22:57:43 -0300

    Symbol table implementation

--- syntax.py
+++ syntax.py
@@ -1,27 +1,74 @@
 ##########################################
 # Nome: Perdo Garcia, Sávio Francisco    #
 # Analisardor Sintático                  #
-#                                        #
 ##########################################
 
 from lexical import LexicalAnalyzer
 import sys
 
+class ASTNode:
+    def __init__(self, node_type, children=None, value=None):
+        self.node_type = node_type
+        self.children = children if children else []
+        self.value = value
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def __str__(self, level=0):
+        indent = "  " * level
+        # Constrói a string de representação do nó atual
+        result = f"{indent}Node(type={self.node_type}, value={self.value})\n"
+        # Recursivamente chama __str__ para cada filho, aumentando o nível de indentação
+        for child in self.children:
+            result += child.__str__(level + 1)
+        return result    
+    
+
+
+class SymbolTable:
+    
+    def __init__(self, func_name):
+        self.symbols = {}
+        self.func_name = func_name
+
+    def add_symbol(self, id:str, tipo:str, pos_par:int):
+        if id in self.symbols:
+            raise Exception(f"Erro: Identificador {id} já declarado.")
+        
+        self.symbols[id] = (tipo, pos_par)
+    
+    def __str__(self) -> str:
+        final_string = f"Tabela da função : {self.func_name} \n"
+        for key in self.symbols.keys(): 
+            final_string += key + ' | ' + str(self.symbols[key]) + '\n'
+        
+        return final_string
+
+
 token_list = LexicalAnalyzer.lexer(sys.argv[1])
 
 line_numb = 1
 
+tabela_de_simbolos = {} #Dicionário das tableas de símbolos
+
+pos_param = 0
+fn_atual = ""
+lista_ids = []
+
 i = 0
 
 global INPUT_TOKEN
+global INPUT_VALUE
 
 for key, value in token_list[i][0].items():
     INPUT_TOKEN = value
+    
     break
 
 def match(EXPECTED_TOKEN):
     
-    global i, token_list, INPUT_TOKEN, line_numb
+    global i, token_list, INPUT_TOKEN, line_numb, INPUT_VALUE
 
     try:
         if token_list[i][2] > token_list[i+1][1]:
@@ -41,6 +88,7 @@ def match(EXPECTED_TOKEN):
                 
                 else:
                     INPUT_TOKEN = key
+                    INPUT_VALUE = value
                 
                 break
 
@@ -49,6 +97,7 @@ def match(EXPECTED_TOKEN):
         exit(-1)
 
 def prog(EXPECTED_TOKEN):
+    
     fun(EXPECTED_TOKEN)
     fun_seq(EXPECTED_TOKEN)
 
@@ -61,24 +110,42 @@ def fun_seq(EXPECTED_TOKEN):
         fun_seq(EXPECTED_TOKEN)
     else:
         return
-      
+
 def fun(EXPECTED_TOKEN):
+    
+    global INPUT_VALUE, fn_atual
+    
     match("FUNCTION")
+
+    #Criara uma nova entrada na trabla de simbolos global
+    tabela_de_simbolos[INPUT_VALUE] = SymbolTable(INPUT_VALUE)
+    fn_atual = INPUT_VALUE
+
+    #cria o no Function
+    func_node = ASTNode("FUNC")
+
     match("Identifier")
     match("L_BRACKET")
     params_list(EXPECTED_TOKEN)
     match("R_BRACKET")
     return_type(EXPECTED_TOKEN)
-    block(EXPECTED_TOKEN)
+    block(EXPECTED_TOKEN, func_node)
+    print(func_node)
 
 def params_list(EXPECTED_TOKEN):
     
-    global INPUT_TOKEN
-
+    global INPUT_TOKEN, INPUT_VALUE, fn_atual, pos_param
+    ##Guardar o lexema do Id
     if ("Identifier" in INPUT_TOKEN):
+        id = INPUT_VALUE
         match("Identifier")
-        match("COLON")
-        type(EXPECTED_TOKEN)
+
+        match("COLON") 
+        tipo = type(EXPECTED_TOKEN)
+        tabela_de_simbolos[fn_atual].add_symbol(id,tipo,pos_param)
+        pos_param+=1
+
+        ##Variável locar para receber 
         params_list_aux(EXPECTED_TOKEN)
     
     else:
@@ -86,13 +153,20 @@ def params_list(EXPECTED_TOKEN):
 
 def params_list_aux(EXPECTED_TOKEN):
     
-    global INPUT_TOKEN
+    global INPUT_TOKEN, INPUT_VALUE, fn_atual, pos_param
 
     if ("COMMA" in INPUT_TOKEN):
+        
         match("COMMA")
+        id = INPUT_VALUE
+        
         match("Identifier")
         match("COLON")
-        type(EXPECTED_TOKEN)
+        tipo = type(EXPECTED_TOKEN)
+        
+        tabela_de_simbolos[fn_atual].add_symbol(id, tipo, pos_param)
+        pos_param+=1
+        
         params_list_aux(EXPECTED_TOKEN)
     
     else:
@@ -109,18 +183,20 @@ def return_type(EXPECTED_TOKEN):
     else:
         return
 
-def block(EXPECTED_TOKEN):
+def block(EXPECTED_TOKEN, func_node : ASTNode):
     match("L_BRACE")
-    seq(EXPECTED_TOKEN)
+    block_node = ASTNode("BLOCK")   #Cria o Node Bloco
+    seq(EXPECTED_TOKEN, block_node) #Passa o Node bloco pra seq
+    func_node.add_child(block_node) #Coloca como filho de function
     match("R_BRACE")
 
-def seq(EXPECTED_TOKEN):
+def seq(EXPECTED_TOKEN, block_node : ASTNode):
     
     global INPUT_TOKEN
 
     if ("LET" in INPUT_TOKEN):
-        declaration(EXPECTED_TOKEN)
-        seq(EXPECTED_TOKEN)
+        declaration(EXPECTED_TOKEN, block_node)
+        seq(EXPECTED_TOKEN, block_node) #passa nó bloco
     
     else:
         if ("Identifier" in INPUT_TOKEN or 
@@ -130,52 +206,67 @@ def seq(EXPECTED_TOKEN):
             "PRINT_LINE" in INPUT_TOKEN or
             "RETURN" in INPUT_TOKEN):
             
-            comands(EXPECTED_TOKEN)
-            seq(EXPECTED_TOKEN)
+            comands(EXPECTED_TOKEN, block_node) #passa nó bloco e retorna
+            seq(EXPECTED_TOKEN, block_node) #passa nó bloco e retorna
     return
 
 def declaration(EXPECTED_TOKEN):
+    global lista_ids, tabela_de_simbolos
     match("LET")
     var_1(EXPECTED_TOKEN)
     match("COLON")
-    type(EXPECTED_TOKEN)
+    tipo = type(EXPECTED_TOKEN)
     match("P_COMMA")
+    for id in lista_ids:
+        tabela_de_simbolos[fn_atual].add_symbol(id,tipo,-1)
+
+    lista_ids = []
 
 def var_1(EXPECTED_TOKEN):
+    global INPUT_TOKEN, INPUT_VALUE
+
+    lista_ids.append(INPUT_VALUE)
+
     match("Identifier")
     var_2(EXPECTED_TOKEN)
 
 def var_2(EXPECTED_TOKEN):
     
-    global INPUT_TOKEN
+    global INPUT_TOKEN, INPUT_VALUE
     
     if ("COMMA" in INPUT_TOKEN):
         match("COMMA")
+        lista_ids.append(INPUT_VALUE)
         match("Identifier")
         var_2(EXPECTED_TOKEN)
     
     return
 
+#Feito
 def type(EXPECTED_TOKEN):
 
     global INPUT_TOKEN
-    
+
     if ("INT" in INPUT_TOKEN):
         match("INT")
+        return "INT"
     
     elif ("FLOAT" in INPUT_TOKEN):
         match("FLOAT")
+        return "FLOAT"
     
     elif ("CHAR" in INPUT_TOKEN):
         match("CHAR")
+        return "CHAR"
 
-def comands(EXPECTED_TOKEN):
+def comands(EXPECTED_TOKEN, block_node : ASTNode):
 
-    global INPUT_TOKEN
+    global INPUT_TOKEN, INPUT_VALUE
 
     if ("Identifier" in INPUT_TOKEN):
+        id = ASTNode("ID", value=INPUT_VALUE)
         match("Identifier")
-        attr_or_call(EXPECTED_TOKEN)
+        attr_or_call(EXPECTED_TOKEN, block_node)
     
     elif ("if" in INPUT_TOKEN):
         if_command(EXPECTED_TOKEN)
@@ -208,15 +299,16 @@ def comands(EXPECTED_TOKEN):
         expression(EXPECTED_TOKEN)
         match("P_COMMA")
 
-def attr_or_call(EXPECTED_TOKEN):
+def attr_or_call(EXPECTED_TOKEN, block_node : ASTNode):
 
     global INPUT_TOKEN
     
     if ("ATTR" in INPUT_TOKEN):
+        attr_node = ASTNode("Assingn")
         match("ATTR")
         expression(EXPECTED_TOKEN)
         match("P_COMMA")
-    
+
     elif ("L_BRACKET" in INPUT_TOKEN):
         match("L_BRACKET")
         args_list(EXPECTED_TOKEN)
@@ -407,3 +499,14 @@ def args_list_2(EXPECTED_TOKEN):
         return
     
 prog("FUNCTION")
+
+for key in tabela_de_simbolos.keys() :
+
+    print(tabela_de_simbolos[key])
+
+##Implementar a tebela
+#1)Como ele é estruturada, p/ cada função tem-se uma tabela
+
+
+##Asa
+##Semântico
